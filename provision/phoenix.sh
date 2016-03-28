40bec991e0b425ca6c7951dd502347f5562feaf7
Melvin Lammerts <melvinrts@gmail.com>
2016-03-28 11:22:27 +0200

    Change sample project dir

--- provision/phoenix.sh
+++ provision/phoenix.sh
@@ -4,6 +4,6 @@ echo "[ ] Installing Phoenix..."
 
 mix local.hex
 echo 'Y' | mix archive.install https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez
-echo 'n' | mix phoenix.new sample_project
+echo 'n' | mix phoenix.new /vagrant/sample_project
 
 echo "[X] Installed Phoenix!"
